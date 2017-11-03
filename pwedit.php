8753e5611a51abfedf1cb7d2a1d80c4757597025
RJHarte <33251295+RJHarte@users.noreply.github.com>
2017-11-03 13:09:15 +0100

    wachtwoord wijzigen

---
+++ pwedit.php
@@ -0,0 +1,37 @@
+<!-- Include file voor bootstrap, stylesheet etc. -->
+<?php include('header_menu.php'); ?>
+
+
+<div class="container">
+  <h1>Wachtwoord wijzigen</h1>
+  <p>  </p>
+
+
+
+	<div class="form-group">
+		<label for="pwd">Vul hier je oude wachtwoord in:</label>
+		<input type="password" class="form-control" id="wwoud">
+	</div>
+	
+	<div class="form-group">
+		<label for="pwd">Vul hier je nieuwe wachtwoord in:</label>
+		<input type="password" class="form-control" id="wwnieuw">
+	</div>
+	
+	<div class="form-group">
+		<label for="pwd">Bevestig je nieuwe wachtwoord</label>
+		<input type="password" class="form-control" id="wwnieuw">
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<div class="pull-right">
+				<button type="submit" class="btn btn-default">Annuleren</button>
+				<button type="submit" class="btn btn-primary">Opslaan</button>
+			</div>
+		</div>
+	</div>
+	
+
+	
+</div
