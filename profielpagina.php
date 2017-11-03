bb1d17ed79165e8b75bb86361960130efe4be16e
RJHarte <33251295+RJHarte@users.noreply.github.com>
2017-11-03 12:42:13 +0100

    profielpagina

---
+++ profielpagina.php
@@ -0,0 +1,146 @@
+<!-- Include file voor bootstrap, stylesheet etc. -->
+<?php include('header_menu.php'); ?>
+		
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<form class="form-horizontal" role="form">
+					<fieldset>
+						
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<div class="pull-left">
+									<a href="orderhistorie.php" class="btn btn-primary">Orderhistorie</a>
+									<a href="wachtwoordwijzig.php" class="btn btn-primary">Wachtwoord wijzigen</a>
+								</div>
+							</div>
+						</div>
+						
+						
+						<legend>Persoonsgegevens</legend>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Voornaam</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Voornaam" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Achternaam</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Achternaam" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Tussenvoegsel</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Tussenvoegsel" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Aanhef</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Aanhef" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Geboortedatum</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Geboortedatum" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Telefoon</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Telefoon" class="form-control">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Mobiel</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Mobiel" class="form-control">
+							</div>
+						</div>
+						  
+						<legend>Adresgegevens</legend>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Plaats</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Plaats" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Straatnaam</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Straatnaam" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Huisnummer</label>
+							<div class="col-sm-4">
+								<input type="text" placeholder="Huisnummer" class="form-control">
+							</div>
+
+							<label class="col-sm-2 control-label" for="textinput">Toevoeging</label>
+							<div class="col-sm-4">
+								<input type="text" placeholder="Toevoeging" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Huisnummer</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Huisnummer" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Toevoeging</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Toevoeging" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="textinput">Postcode</label>
+							<div class="col-sm-10">
+								<input type="text" placeholder="Postcode" class="form-control">
+							</div>
+						</div>
+						  
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<div class="pull-right">
+									<button type="submit" class="btn btn-default">Annuleren</button>
+									<button type="submit" class="btn btn-primary">Opslaan</button>
+								</div>
+							</div>
+						</div>
+				  		 
+					</fieldset>
+				</form>
+			</div>
+		</div>
+		
+	</div>
+	<!-- Footer inhoud -->
+	<footer class="row">
+		<div class="col-sm-12">
+			<p>Footer tekst.....</p>
+		</div>
+	</footer>
+	<!-- Einde footer inhoud -->
+
+	<!-- Container en body afsluiten -->
+	</div>
+</body>
+
+	
