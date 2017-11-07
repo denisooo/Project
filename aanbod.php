7e9d07d50c5e00f82201643f5ae4ffca0b0f7bcc
Gertjan <gertjanhaan@live.nl>
2017-11-07 12:04:25 +0100

    aanbodpagina

--- aanbod.php
+++ aanbod.php
@@ -7,7 +7,7 @@
 	<!-- Categorieen menu -->
 	<div class="col-sm-3">
 		<?php
-			$query2 = "SELECT Product_id, Categorie_naam, Naam_product, Beschrijving, Gewicht, Vraagprijs FROM product p join categorie c ON p.categorie_id = c.categorie_id WHERE Categorie_naam = '".$_GET[ 'Categorie_naam']."'";
+			$query2 = "SELECT Product_id, Categorie_naam, Naam_product, Beschrijving, Gewicht, Vraagprijs FROM product p join categorie c ON p.categorie_id = c.categorie_id WHERE Categorie_naam = '" . $_GET[ 'Categorie_naam'] . "'";
 			$query ="SELECT Categorie_naam FROM categorie";
 			$results=mysqli_query($db, $query);
 			if($results){
@@ -18,11 +18,11 @@
 		?>
 
 		<ul class="nav nav-pills nav-stacked" role="tablist">
-			<li><a href="aanbod.php">Alle producten</a></li>
+			<li><a href="aanbod.php?Categorie_naam=test">Alle producten</a></li>
 			<?php
 				while($row=mysqli_fetch_assoc($results)) {
 			?>
-					<li><a href="aanbod.php?<?php echo $row['Categorie_naam'];?>">
+					<li><a href="aanbod.php?Categorie_naam=<?php echo $row['Categorie_naam'];?>">
 						<?php echo $row['Categorie_naam'];?>
 					</a></li>
 			<?php
@@ -33,92 +33,151 @@
 	<!-- Catalogus -->
 	<div class="col-sm-9">
 		<?php
-		if ($_GET['Categorie_naam'] = "Dieren"){
-			$results2 = mysqli_query($db, $query2);
-			if($num > 0){
-				while($row=mysqli_fetch_assoc($results2)) {
-					?><td><?php echo $row['Naam_product'];
-					echo $row['Beschrijving'];
-					echo $row['Categorie_naam'];
-					echo $row['Gewicht'];
-					echo $row['Vraagprijs'];?>
-				</td>
-		<?php
-	}
-}
-		}
-			elseif ($_GET['Categorie_naam'] = "Vlaggen") {
-			$results2 = mysqli_query($db, $query2);
-			if($num > 0){
-				while($row=mysqli_fetch_assoc($results2)) {
-					?><td><?php echo $row['Naam_product'];
-					echo $row['Beschrijving'];
-					echo $row['Categorie_naam'];
-					echo $row['Gewicht'];
-					echo $row['Vraagprijs'];?>
-				</td>
-		<?php
-	}
-}
-		}
-			elseif ($_GET['Categorie_naam'] = "Stripfiguren") {
-			$results2 = mysqli_query($db, $query2);
-			if($num > 0){
-				while($row=mysqli_fetch_assoc($results2)) {
-					?><td><?php echo $row['Naam_product'];
-					echo $row['Beschrijving'];
-					echo $row['Categorie_naam'];
-					echo $row['Gewicht'];
-					echo $row['Vraagprijs'];?>
-				</td>
-		<?php
-	}
-}
-		}
-			elseif ($_GET['Categorie_naam'] = "TV-series") {
-			$results = mysqli_query($db, $query2);
-			if($num > 0){
-				while($row=mysqli_fetch_assoc($results2)) {
-					?><td><?php echo $row['Naam_product'];
-					echo $row['Beschrijving'];
-					echo $row['Categorie_naam'];
-					echo $row['Gewicht'];
-					echo $row['Vraagprijs'];?>
-				</td>
-		<?php
-	}
-}
-		}
-			elseif ($_GET['Categorie_naam'] = "Automerken") {
-			$results2 = mysqli_query($db, $query2);
-			if($num > 0){
-				while($row=mysqli_fetch_assoc($results2)) {
-					?><td><?php echo $row['Naam_product'];
-					echo $row['Beschrijving'];
-					echo $row['Categorie_naam'];
-					echo $row['Gewicht'];
-					echo $row['Vraagprijs'];?>
-				</td>
-		<?php
-	}
-}
-		}
-			else {
-        $query2 = "SELECT Product_id, Categorie_naam, naam_product, Beschrijving, Gewicht, Vraagprijs FROM product p JOIN categorie c ON p.Categorie_id = c.Categorie_id ";
+			if ($_GET['Categorie_naam'] = "Dieren"){
 				$results2 = mysqli_query($db, $query2);
+				if($num > 0){?>
+					<div class="row">
+					<?php while($row=mysqli_fetch_assoc($results2)) {?>
+						<div class="col-xs-4">
+							<div class="thumbnail">
+								<a href="#">
+									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
+									<div class="caption">
+										<strong><?php echo $row['Naam_product']?></strong><br>												
+										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br><br>
+										<a href="#" class="btn btn-success btn-block">
+											<span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen
+										</a><br><br>
+										<p><em><?php echo $row['Beschrijving']?></em></p>
+									</div>
+								</a>
+							</div>
+						</div>
+					<?php }
+				} ?>
+					</div>
+			<?php }
+			
+			elseif ($_GET['Categorie_naam'] = "Vlaggen"){
+				$results2 = mysqli_query($db, $query2);
+				if($num > 0){?>
+					<div class="row">
+					<?php while($row=mysqli_fetch_assoc($results2)) {?>
+						<div class="col-xs-4">
+							<div class="thumbnail">
+								<a href="#">
+									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
+									<div class="caption">
+										<strong><?php echo $row['Naam_product']?></strong><br>												
+										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br><br>
+										<a href="#" class="btn btn-success btn-block">
+											<span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen
+										</a><br><br>
+										<p><em><?php echo $row['Beschrijving']?></em></p>
+									</div>
+								</a>
+							</div>
+						</div>
+					<?php }
+				} ?>
+					</div>
+			<?php }
+			
+			elseif ($_GET['Categorie_naam'] = "Stripfiguren"){
+				$results2 = mysqli_query($db, $query2);
+				if($num > 0){?>
+					<div class="row">
+					<?php while($row=mysqli_fetch_assoc($results2)) {?>
+						<div class="col-xs-4">
+							<div class="thumbnail">
+								<a href="#">
+									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
+									<div class="caption">
+										<strong><?php echo $row['Naam_product']?></strong><br>												
+										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br><br>
+										<a href="#" class="btn btn-success btn-block">
+											<span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen
+										</a><br><br>
+										<p><em><?php echo $row['Beschrijving']?></em></p>
+									</div>
+								</a>
+							</div>
+						</div>
+					<?php }
+				} ?>
+					</div>
+			<?php }
+			
+			elseif ($_GET['Categorie_naam'] = "TV-series"){
+				$results2 = mysqli_query($db, $query2);
+				if($num > 0){?>
+					<div class="row">
+					<?php while($row=mysqli_fetch_assoc($results2)) {?>
+						<div class="col-xs-4">
+							<div class="thumbnail">
+								<a href="#">
+									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
+									<div class="caption">
+										<strong><?php echo $row['Naam_product']?></strong><br>												
+										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br><br>
+										<a href="#" class="btn btn-success btn-block">
+											<span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen
+										</a><br><br>
+										<p><em><?php echo $row['Beschrijving']?></em></p>
+									</div>
+								</a>
+							</div>
+						</div>
+					<?php }
+				} ?>
+					</div>
+			<?php }
+			
+			elseif ($_GET['Categorie_naam'] = "Automerken"){
+				$results2 = mysqli_query($db, $query2);
+				if($num > 0){?>
+					<div class="row">
+					<?php while($row=mysqli_fetch_assoc($results2)) {?>
+						<div class="col-xs-4">
+							<div class="thumbnail">
+								<a href="#">
+									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
+									<div class="caption">
+										<strong><?php echo $row['Naam_product']?></strong><br>												
+										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br><br>
+										<a href="#" class="btn btn-success btn-block">
+											<span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen
+										</a><br><br>
+										<p><em><?php echo $row['Beschrijving']?></em></p>
+									</div>
+								</a>
+							</div>
+						</div>
+					<?php }
+				} ?>
+					</div>
+			<?php }
+			
+			elseif ($_GET['Categorie_naam'] = "test") {
+				echo "test";
+
+				$query3 = "SELECT Product_id, Categorie_naam, Naam_product, Beschrijving, Gewicht, Vraagprijs FROM product p join categorie c ON p.categorie_id = c.categorie_id";
+
+				$results2 = mysqli_query($db, $query3);
 				if($num > 0){
-    			while($row=mysqli_fetch_assoc($results2)) {
-						?><td><?php echo $row['Naam_product'];
-						echo $row['Beschrijving'];
-						echo $row['Categorie_naam'];
-						echo $row['Gewicht'];
-						echo $row['Vraagprijs'];?>
-					</td>
-			<?php
-		}
-}
-}
-?>
+					while($row=mysqli_fetch_assoc($results2)) {
+						?><td><?php 
+							echo $row['Naam_product'];
+							echo $row['Beschrijving'];
+							echo $row['Categorie_naam'];
+							echo $row['Gewicht'];
+							echo $row['Vraagprijs'];?>
+						</td>
+						<?php
+					}
+				}
+			}?>
+			
 	</div>
 </div>
 <!-- Einde catalogus inhoud -->
