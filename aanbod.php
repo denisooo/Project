948ac983893d01bb95df5052f168800c02402340
Gertjan <gertjanhaan@live.nl>
2017-11-08 13:31:27 +0100

    aanbod

--- aanbod.php
+++ aanbod.php
@@ -19,7 +19,6 @@
 		?>
 
 		<ul class="nav nav-pills nav-stacked" role="tablist">
-			<li><a href="aanbod.php?Categorie_naam=Alle_producten">Alle producten</a></li>
 			<?php
 				while($row=mysqli_fetch_assoc($results)) {
 			?>
@@ -34,32 +33,7 @@
 	
 	<!-- Catalogus -->
 	<div class="col-sm-9">
-		<?php if ($_GET['Categorie_naam'] = "Alle_producten"){
-			$results3 = mysqli_query($db, $query3);
-			if($num > 0){?>
-				<div class="row">
-					<?php while($row=mysqli_fetch_assoc($results3)) {?>
-						<div class="col-xs-4">
-							<div class="thumbnail">
-								<form action="toevoegen.php" method="post">;
-									<img src="fotos/<?php echo $row['Product_id']?>_1.png" alt="Afbeelding niet beschikbaar" style="width:100%">
-									<div class="caption">
-										<input type="hidden" name="prod_id" value="<?php echo $row['Product_id']?>" />
-										<strong><?php echo $row['Naam_product']?></strong><br>	
-										Vraagprijs: <?php echo "€" . $row['Vraagprijs']?><br>
-										Aantal: <input type="text" name="aantal_prod" size="2" maxlength="2" value="1" /><br><br>
-										<input class="btn btn-success btn-block" type="submit" value="Winkelwagen" /> 
-										<p><em><?php echo $row['Beschrijving']?></em></p>
-									</div>
-								</form>
-							</div>
-						</div>
-					<?php } ?>
-				</div>
-			<?php }
-		} 
-	
-		elseif ($_GET['Categorie_naam'] = "Dieren"){
+		<?php if ($_GET['Categorie_naam'] = "Dieren"){
 			$results2 = mysqli_query($db, $query2);
 			if($num > 0){?>
 				<div class="row">
